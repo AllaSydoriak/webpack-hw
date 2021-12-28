745618052c2dd167f884a6ec6f394da90996319f
AllaSydoriak <alochka12@ukr.net>
2021-12-28 22:08:35 +0200

    basic webpack configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classNames from './css-modules.css';
+
+export default () => (
+    <>
+        <header className={classNames.header}>
+            Header
+        </header>
+        <content>
+            <div>Simple app</div>
+        </content>
+    </>
+);
